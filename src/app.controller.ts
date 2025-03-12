b82dedaf8fc9296f8046d491843fd4a4f0addce9
juancho391 <juanchischis2010@hotmail.com>
2025-03-12 15:56:44 -0500

    aprendiendo a definir nuevos endpoints y recibir parametros en la url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,21 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello World!!!!';
+  }
+
+  //Asi recibimos un parametro
+  @Get('/products/:id')
+  getProducts(@Param('id') id: number): string {
+    return `Product with id: ${id}`;
+  }
+
+  //Asi recibo 2 parametros
+  @Get('/categories/:categoryId/products/:productId')
+  getCategory(
+    @Param('categoryId') categoryId: number,
+    @Param('productId') productId: number,
+  ): string {
+    return `category with id: ${categoryId} and product with id : ${productId}`;
   }
 }
