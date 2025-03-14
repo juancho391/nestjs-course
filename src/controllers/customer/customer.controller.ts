2ba847b6a5c32a58b32aab70b49c05d6d1cca37b
juancho391 <juanchischis2010@hotmail.com>
2025-03-14 09:32:28 -0500

    Aprendiendo a implementar metodos put y delete, ademas a responder con json y enviar HttpStatusCode

--- src/controllers/customer/customer.controller.ts
+++ src/controllers/customer/customer.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+  @Get()
+  getCustomers(): string {
+    return 'Welcome to customers';
+  }
+
+  @Get(':id')
+  getCustomer(@Param('id') id: number): string {
+    return `Customer with id => ${id}`;
+  }
+}
--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -21,5 +32,39 @@ export class ProductsController {
   @Get('/:id')
   getProduct(@Param('id') id: number): string {
     return `Product with id: ${id}`;
+  }
+
+  //Metodo post
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createProduct(@Body() payload: { name: string; price: number }): any {
+    return {
+      message: 'Product Created',
+      payload,
+    };
+  }
+
+  //Metodo Put para actualizar
+  @Put('/:id')
+  @HttpCode(HttpStatus.ACCEPTED)
+  updateProduct(
+    @Param('id') id: number,
+    @Body() payload: { name?: string; price?: number },
+  ): any {
+    return {
+      message: 'Product Updated',
+      id,
+      payload,
+    };
+  }
+
+  //Metodo delete
+  @Delete('/:id')
+  @HttpCode(HttpStatus.ACCEPTED)
+  deleteProduct(@Param('id') id: number): any {
+    return {
+      message: 'Product deleted',
+      id,
+    };
   }
 }
