2129a7982427c0aea4574d0db09d8218f18412dd
juancho391 <juanchischis2010@hotmail.com>
2025-03-20 19:24:02 -0500

    usando el httpmodule para consumir apis desde nestjs e injectando esto al appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,36 @@
 import { Module } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { HttpModule } from '@nestjs/axios';
+import { HttpService } from '@nestjs/axios';
+
+const API_KEY = '123456';
+const API_KEY_PROD = '123456789';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      // useValue: API_KEY,
+      useValue: process.env.NODE_ENV === 'production' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (https: HttpService) => {
+        const response = https.get<{ data: any }>(
+          'https://jsonplaceholder.typicode.com/todos',
+        );
+        const tasks = await firstValueFrom(response);
+        return tasks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
